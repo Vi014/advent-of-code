fe3180904c7ad2ecea03e126ea7fc9eb535714dd
Vi014 <78529637+Vi014@users.noreply.github.com>
2023-12-05 04:56:12 +0100

    2023 day 2

---
+++ 2023/02/Part1/Program.cs
@@ -0,0 +1,39 @@
+﻿int sum = 0;
+string[] lines = File.ReadAllLines("input");
+for(int i = 0; i < lines.Count(); i++)
+{
+    string line = lines[i].Split(':')[1];
+    if(check(line)) sum += i + 1;
+}
+
+System.Console.WriteLine(sum.ToString());
+
+bool check(string inputText)
+{
+    string[] draws = inputText.Split(';');
+
+    foreach(string draw in draws)
+    {
+        string[] colors = draw.Split(',');
+
+        foreach(string color in colors)
+        {
+            string[] counts = color.Trim().Split(' ');
+
+            switch(counts[1])
+            {
+                case "red":
+                    if(Convert.ToInt32(counts[0].Trim()) > 12) return false;
+                    break;
+                case "green":
+                    if(Convert.ToInt32(counts[0].Trim()) > 13) return false;
+                    break;
+                case "blue":
+                    if(Convert.ToInt32(counts[0].Trim()) > 14) return false;
+                    break;
+            }
+        }
+    }
+
+    return true;
+}
---
+++ 2023/02/Part2/Program.cs
@@ -0,0 +1,41 @@
+﻿int sum = 0;
+string[] lines = File.ReadAllLines("input");
+for(int i = 0; i < lines.Count(); i++)
+{
+    string line = lines[i].Split(':')[1];
+    sum += calcPow(line);
+}
+
+System.Console.WriteLine(sum.ToString());
+
+int calcPow(string inputText)
+{
+    int maxR = 0, maxG = 0, maxB = 0;
+
+    string[] draws = inputText.Split(';');
+
+    foreach(string draw in draws)
+    {
+        string[] colors = draw.Split(',');
+
+        foreach(string color in colors)
+        {
+            string[] counts = color.Trim().Split(' ');
+
+            switch(counts[1])
+            {
+                case "red":
+                    if(Convert.ToInt32(counts[0].Trim()) > maxR) maxR = Convert.ToInt32(counts[0].Trim());
+                    break;
+                case "green":
+                    if(Convert.ToInt32(counts[0].Trim()) > maxG) maxG = Convert.ToInt32(counts[0].Trim());
+                    break;
+                case "blue":
+                    if(Convert.ToInt32(counts[0].Trim()) > maxB) maxB = Convert.ToInt32(counts[0].Trim());
+                    break;
+            }
+        }
+    }
+
+    return maxR * maxG * maxB;
+}
