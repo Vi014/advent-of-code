e23e5b3f3fa5c92b738f8df81ff6d8f429fbf763
Vi014 <78529637+Vi014@users.noreply.github.com>
2023-12-06 18:25:00 +0100

    2023 day 4

---
+++ 2023/04/Part1/Program.cs
@@ -0,0 +1,29 @@
+﻿int sum = 0;
+string[] lines = File.ReadAllLines("input");
+foreach(string line in lines)
+{
+    string[] numbers = line.Split(':')[1].Split('|');
+    numbers[0] = numbers[0].Trim();
+    numbers[1] = numbers[1].Trim();
+    string[] winNrs = numbers[0].Split(' ');
+    string[] gotNrs = numbers[1].Split(' ');
+
+    int hits = 0;
+    foreach(string winNr in winNrs)
+    {
+        if(winNr.Length != 0)
+        {
+            foreach(string gotNr in gotNrs)
+            {
+                if(gotNr.Length != 0)
+                {
+                    if(winNr == gotNr) hits++;
+                }
+            }
+        }
+    }
+    
+    if(hits != 0) sum += Convert.ToInt32(Math.Pow(2, hits - 1));
+}
+
+System.Console.WriteLine(sum.ToString());
---
+++ 2023/04/Part2/Program.cs
@@ -0,0 +1,42 @@
+﻿int sum = 0;
+string[] lines = File.ReadAllLines("input");
+int[] cardInstances = new int[lines.Length];
+
+for (int i = 0; i < cardInstances.Length; i++)
+{
+    cardInstances[i] = 1;
+}
+
+for(int i = 0; i < lines.Length; i++)
+{
+    string line = lines[i];
+    string[] numbers = line.Split(':')[1].Split('|');
+    numbers[0] = numbers[0].Trim();
+    numbers[1] = numbers[1].Trim();
+    string[] winNrs = numbers[0].Split(' ');
+    string[] gotNrs = numbers[1].Split(' ');
+
+    int hits = 0;
+    foreach(string winNr in winNrs)
+    {
+        if(winNr.Length != 0)
+        {
+            foreach(string gotNr in gotNrs)
+            {
+                if(gotNr.Length != 0)
+                {
+                    if(winNr == gotNr) hits++;
+                }
+            }
+        }
+    }
+
+    for(int j = i+1; j <= i+hits; j++)
+    {
+        cardInstances[j] += cardInstances[i];
+    }
+    
+    sum += cardInstances[i];
+}
+
+System.Console.WriteLine(sum.ToString());
