0fb5c4eab35b20264025679ec3085c4c6b65496a
Vi014 <78529637+Vi014@users.noreply.github.com>
2023-12-12 07:09:21 +0100

    2023 day 6

---
+++ 2023/06/Part1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+string[] lines = File.ReadAllLines("input");
+int[,] values = new int[lines.Length, (Regex.Split(lines[0], @"\s+").Length)-1];
+
+for(int i = 0; i < lines.Length; i++)
+{
+    string[] numbers = Regex.Split(lines[i], @"\s+");
+    for(int j = 1; j < numbers.Length; j++) values[i, j-1] = int.Parse(numbers[j]);
+}
+
+int product = 1;
+for(int i = 0; i < values.GetLength(1); i++)
+{
+    int wayCnt = 0;
+
+    for(int j = 1; j < values[0, i]; j++) 
+    {
+        if(j * (values[0, i]-j) > values[1, i]) wayCnt++;
+    }
+
+    product *= wayCnt;
+}
+
+System.Console.WriteLine($"{product}");
---
+++ 2023/06/Part2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+string[] lines = File.ReadAllLines("input");
+long time = 0, distance = 0;
+
+for(int i = 0; i < lines.Length; i++)
+{
+    string[] numbers = Regex.Split(lines[i], @"\s+");
+    string number = "";
+    for(int j = 1; j < numbers.Length; j++) number += numbers[j];
+    if(i == 0) time = long.Parse(number);
+    else distance = long.Parse(number);
+}
+
+long min = 0, max = 0; 
+
+for(long i = 1; min == 0; i++)
+    if(i * (time - i) > distance) min = i;
+
+for(long i = time - 1; max == 0; i--)
+    if(i * (time - i) > distance) max = i;
+
+System.Console.WriteLine($"{max-min+1}");
