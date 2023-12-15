d2e98aa109d7f50ca4c1ac1c478d45e678558bd9
Vi014 <78529637+Vi014@users.noreply.github.com>
2023-12-15 14:01:15 +0100

    2023 day 9 part 2

---
+++ 2023/09/Part2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+string[] lines = File.ReadAllLines("input");
+long sum = 0;
+
+foreach(string line in lines)
+{
+    string[] numbersS = line.Split(" ");
+    long[] numbersL = new long[numbersS.Length];
+    for(int i = 0; i < numbersS.Length; i++)
+        numbersL[i] = long.Parse(numbersS[i]);
+
+    sum += predictValue(numbersL);
+}
+
+System.Console.WriteLine($"{sum}");
+
+long predictValue(long[] inputArr)
+{
+    long[] nextStep = new long[inputArr.Length - 1];
+    bool lastStep = true;
+
+    for(int i = 0; i < nextStep.Length; i++)
+    {
+        nextStep[i] = inputArr[i+1] - inputArr[i];
+        if(nextStep[i] != 0) lastStep = false;
+    }
+
+    if(!lastStep) return inputArr[0] - predictValue(nextStep);
+    else return inputArr[inputArr.Length - 1];
+}
