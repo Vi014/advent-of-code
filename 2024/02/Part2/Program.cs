84ad848e387ab9792844d20d66609ae303fdc935
Vi014 <78529637+Vi014@users.noreply.github.com>
2024-12-02 21:42:47 +0100

    2024 day 02 part 2

---
+++ 2024/02/Part2/Program.cs
@@ -0,0 +1,56 @@
+﻿var lines = File.ReadAllLines("input");
+var count = 0;
+
+foreach (var line in lines)
+{
+    var levels = line.Split(' ');
+
+    if (CheckSafety(levels))
+    {
+        count++;
+    }
+    else
+    {
+        for (var i = 0; i < levels.Length; i++)
+        {
+            var levelShortened = new List<string>(levels);
+            levelShortened.RemoveAt(i);
+            if (CheckSafety(levelShortened.ToArray()))
+            {
+                count++;
+                break;
+            }
+        }
+    }
+}
+
+Console.WriteLine($"{count}");
+return;
+
+bool CheckSafety(string[] levels)
+{
+    if (int.Parse(levels[0]) > int.Parse(levels[1]))
+    {
+        // all decreasing
+        for (var i = 0; i < levels.Length - 1; i++)
+        {
+            var curr = int.Parse(levels[i]);
+            var next = int.Parse(levels[i + 1]);
+            if (curr - next >= 1 && curr - next <= 3) continue;
+            return false;
+        }
+    }
+    else
+    {
+        // all increasing
+        for (var i = 0; i < levels.Length - 1; i++)
+        {
+            var curr = int.Parse(levels[i]);
+            var next = int.Parse(levels[i + 1]);
+            if (next - curr >= 1 && next - curr <= 3) continue;
+            return false;
+        }
+    }
+    
+    return true;
+}
