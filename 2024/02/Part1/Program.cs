09774113a5523b2babfe96675eafd5f8afecdacb
Vi014 <78529637+Vi014@users.noreply.github.com>
2024-12-02 07:59:04 +0100

    2024 day 02 part 1

---
+++ 2024/02/Part1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.AccessControl;
+
+var lines = File.ReadAllLines("input");
+var count = 0;
+
+foreach (var line in lines)
+{
+    var levels = line.Split(' ');
+    var safe = true;
+
+    if (int.Parse(levels[0]) > int.Parse(levels[1]))
+    {
+        // all decreasing
+        for (var i = 0; i < levels.Length - 1; i++)
+        {
+            var curr = int.Parse(levels[i]);
+            var next = int.Parse(levels[i + 1]);
+            if (curr - next < 1 || curr - next > 3)
+            {
+                safe = false;
+                break;
+            }
+        }
+    }
+    else
+    {
+        // all increasing
+        for (var i = 0; i < levels.Length - 1; i++)
+        {
+            var curr = int.Parse(levels[i]);
+            var next = int.Parse(levels[i + 1]);
+            if (next - curr < 1 || next - curr > 3)
+            {
+                safe = false;
+                break;
+            }
+        }
+    }
+
+    if (safe)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"{count}");
