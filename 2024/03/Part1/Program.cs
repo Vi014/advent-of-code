0d853813abddb86014213a29eb432269c57e2676
Vi014 <78529637+Vi014@users.noreply.github.com>
2024-12-09 07:59:45 +0100

    2024 day 03

---
+++ 2024/03/Part1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+var lines = File.ReadAllLines("input");
+var regex = new Regex(@"mul\(\d{1,3},\d{1,3}\)");
+var sum = 0;
+
+foreach (var line in lines)
+{
+    var matches = regex.Matches(line);
+    foreach (var match in matches)
+    {
+        var regex2 = new Regex(@"\d{1,3}");
+        var matches2 = regex2.Matches(match.ToString()!);
+        sum += Convert.ToInt32(matches2[0].Value) * Convert.ToInt32(matches2[1].Value);
+    }
+}
+
+Console.WriteLine($"{sum}");
---
+++ 2024/03/Part2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+using System.Threading.Channels;
+
+var lines = File.ReadAllLines("input");
+var regex = new Regex(@"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)");
+var sum = 0;
+var enabled = true;
+
+foreach (var line in lines)
+{
+    var matches = regex.Matches(line);
+    foreach (var match in matches)
+    {
+        if (match.ToString() == "do()") enabled = true;
+        else if (match.ToString() == "don't()") enabled = false;
+        else if (enabled)
+        {
+            var regex2 = new Regex(@"\d{1,3}");
+            var matches2 = regex2.Matches(match.ToString()!);
+            sum += Convert.ToInt32(matches2[0].Value) * Convert.ToInt32(matches2[1].Value);
+        }
+    }
+}
+
+Console.WriteLine($"{sum}");
