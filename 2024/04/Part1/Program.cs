c9cab2617f474287948adaafee027daa5f315893
Vi014 <78529637+Vi014@users.noreply.github.com>
2024-12-09 08:54:11 +0100

    2024 day 04

---
+++ 2024/04/Part1/Program.cs
@@ -0,0 +1,40 @@
+﻿var lines = File.ReadAllLines("input");
+var linesCnt = lines.Length;
+var count = 0;
+
+for (var i = 0; i < linesCnt; i++)
+{
+    var line = lines[i];
+    var lineCnt = line.Length;
+
+    for (var j = 0; j < lineCnt; j++)
+    {
+        if (lines[i][j] == 'X')
+        {
+            if (i >= 3)
+            {
+                if (lines[i - 1][j] == 'M' && lines[i - 2][j] == 'A' && lines[i - 3][j] == 'S') count++;
+                if (j >= 3)
+                    if (lines[i - 1][j - 1] == 'M' && lines[i - 2][j - 2] == 'A' && lines[i - 3][j - 3] == 'S') count++;
+                if (j + 3 < lineCnt)
+                    if (lines[i - 1][j + 1] == 'M' && lines[i - 2][j + 2] == 'A' && lines[i - 3][j + 3] == 'S') count++;
+            }
+            
+            if(j >= 3)
+                if(lines[i][j - 1] == 'M' && lines[i][j - 2] == 'A' && lines[i][j - 3] == 'S') count++;
+            if(j + 3 < lineCnt)
+                if(lines[i][j + 1] == 'M' && lines[i][j + 2] == 'A' && lines[i][j + 3] == 'S') count++;
+            
+            if (i + 3 < linesCnt)
+            {
+                if (lines[i + 1][j] == 'M' && lines[i + 2][j] == 'A' && lines[i + 3][j] == 'S') count++;
+                if (j >= 3)
+                    if (lines[i + 1][j - 1] == 'M' && lines[i + 2][j - 2] == 'A' && lines[i + 3][j - 3] == 'S') count++;
+                if (j + 3 < lineCnt)
+                    if (lines[i + 1][j + 1] == 'M' && lines[i + 2][j + 2] == 'A' && lines[i + 3][j + 3] == 'S') count++;
+            }
+        }
+    }
+}
+
+Console.WriteLine(count);
---
+++ 2024/04/Part2/Program.cs
@@ -0,0 +1,21 @@
+﻿var lines = File.ReadAllLines("input");
+var linesCnt = lines.Length;
+var count = 0;
+
+for (var i = 1; i < linesCnt - 1; i++)
+{
+    var line = lines[i];
+    var lineCnt = line.Length;
+
+    for (var j = 1; j < lineCnt - 1; j++)
+    {
+        if (lines[i][j] == 'A')
+        {
+            if ((lines[i-1][j-1] == 'M' && lines[i+1][j+1] == 'S') || (lines[i-1][j-1] == 'S' && lines[i+1][j+1] == 'M'))
+                if ((lines[i+1][j-1] == 'M' && lines[i-1][j+1] == 'S') || (lines[i+1][j-1] == 'S' && lines[i-1][j+1] == 'M'))
+                    count++;
+        }
+    }
+}
+
+Console.WriteLine(count);
