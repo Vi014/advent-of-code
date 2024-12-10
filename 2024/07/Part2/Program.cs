5e145f612380d690fd9662d905939b8b8783f83c
Vi014 <78529637+Vi014@users.noreply.github.com>
2024-12-10 10:26:50 +0100

    2024 day 07 part 2

---
+++ 2024/07/Part2/Program.cs
@@ -0,0 +1,55 @@
+﻿var lines = File.ReadAllLines("input");
+var numbers = new List<List<long>>();
+
+foreach (var line in lines)
+{
+    var split1 = line.Split(':');
+    var split2 = split1[1].Split(' ');
+
+    var nums = new List<long>();
+    nums.Add(long.Parse(split1[0]));
+
+    for (var i = 1; i < split2.Length; i++)
+    {
+        nums.Add(long.Parse(split2[i]));
+    }
+    
+    numbers.Add(nums);
+}
+
+long sum = 0;
+foreach (var number in numbers)
+{
+    if (Calculate(number) == number[0]) sum += number[0];
+}
+Console.WriteLine(sum);
+return;
+
+
+
+long Calculate(List<long> inputList)
+{
+    if (inputList.Count == 3)
+    {
+        if (inputList[1] + inputList[2] == inputList[0]) return inputList[0];
+        if (inputList[1] * inputList[2] == inputList[0]) return inputList[0];
+        return long.Parse($"{inputList[1]}{inputList[2]}");
+    }
+    else
+    {
+        var listAdd = inputList.ToList();
+        listAdd[1] += inputList[2];
+        listAdd.RemoveAt(2);
+        if (Calculate(listAdd) == inputList[0]) return inputList[0];
+        
+        var listMul = inputList.ToList();
+        listMul[1] *= inputList[2];
+        listMul.RemoveAt(2);
+        if (Calculate(listMul) == inputList[0]) return inputList[0];
+        
+        var listCnc = inputList.ToList();
+        listCnc[1] = long.Parse($"{inputList[1]}{inputList[2]}");
+        listCnc.RemoveAt(2);
+        return Calculate(listCnc);
+    }
+}
